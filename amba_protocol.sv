31ad8fdf3e76e988992a2c5e7b0ca3c59ffa7a0c
Talita Valeria <talita.v.matta@gmail.com>
2017-03-27 16:25:10 -0300

    Modificação amba_protocol

--- amba_protocol.sv
+++ amba_protocol.sv
@@ -20,48 +20,44 @@ output logic [DATA_WIDTH-1:0] DATA_OUT
   logic [DATA_WIDTH-1:0] BSG_DATA_1;
   
   
-  always @(posedge G_CLK_TX or negedge rst)
-    begin
+always @(posedge G_CLK_TX or negedge rst)
+begin
       
-      if(~rst)
-        begin
-         BSG_CONTROL <= 'd0;
-        	BSG_DATA_0 <= 'd0;
-        	BSG_DATA_1 <= 'd0;
-			DATA_OUT <= 'd0;
-        end
-    end
-
-    always @(posedge G_CLK_TX)
-    begin
-      if(WRITE_ENABLE == 1)
-			case (ADDR_IN)
-				8'h10: 
-					  DATA_OUT <= BSG_CONTROL;
-				8'h11:
-					  DATA_OUT <= BSG_DATA_0;
-				8'h12:
-					  DATA_OUT <= BSG_DATA_1;
-				default:
-						DATA_OUT <= 'd0;
-			endcase
- 		else
-		  case (ADDR_IN)
-				8'h10: 
-					  BSG_CONTROL <= DATA_IN;
-				8'h11:
-					  BSG_DATA_0 <= DATA_IN;
-				8'h12:
-					  BSG_DATA_1 <= DATA_IN;
-				default:
-					begin
-						BSG_CONTROL <= 'd0;
-						BSG_DATA_0 <= 'd0;
-						BSG_DATA_1 <= 'd0;
-					end
-		endcase
-		
+	if(~rst)
+	begin
+		BSG_CONTROL <= 'd0;
+		BSG_DATA_0 <= 'd0;
+		BSG_DATA_1 <= 'd0;
+		DATA_OUT <= 'd0;
 	end
+	else if(WRITE_ENABLE == 1)
+		case (ADDR_IN)
+			8'h10: 
+				  DATA_OUT <= BSG_CONTROL;
+			8'h11:
+				  DATA_OUT <= BSG_DATA_0;
+			8'h12:
+				  DATA_OUT <= BSG_DATA_1;
+			/*default: Nao pode ter esse default, porque o endereço eh compartilhado com a TPU e o BD
+					DATA_OUT <= 'd0;*/
+		endcase
+	else
+	  case (ADDR_IN)
+			8'h10: 
+				  BSG_CONTROL <= DATA_IN;
+			8'h11:
+				  BSG_DATA_0 <= DATA_IN;
+			8'h12:
+				  BSG_DATA_1 <= DATA_IN;
+			/*default: Nao pode ter esse default, porque o endereço eh compartilhado com a TPU e o BD
+				begin
+					BSG_CONTROL <= 'd0;
+					BSG_DATA_0  <= 'd0;
+					BSG_DATA_1  <= 'd0;
+				end*/
+	endcase
+		
+end
 	 
 endmodule
 
